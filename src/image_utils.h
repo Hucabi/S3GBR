189ba8efb1af6223a65bbd233b09a2a525e88bae
Noel.fasquel <wachirawit.fasquel@epita.fr>
2025-10-31 17:06:04 +0100

    80lignes

--- src/image_utils.h
+++ src/image_utils.h
@@ -1,9 +1,13 @@
 #ifndef IMAGE_UTILS_H
 #define IMAGE_UTILS_H
 
-void rgb_to_gray(unsigned char* img, unsigned char* gray, int width, int height, int channels);
-void compute_histogram(unsigned char* gray, int width, int height, int hist[256]);
-int otsu_threshold(unsigned char* gray, int width, int height);
-void binarize(unsigned char* gray, unsigned char* bw, int width, int height);
+void rgb_to_gray(unsigned char* img, unsigned char* gray, int width,
+	       	int height, int channels);
+void compute_histogram(unsigned char* gray, int width,
+	       	int height, int hist[256]);
+int otsu_threshold(unsigned char* gray, int width,
+	       	int height);
+void binarize(unsigned char* gray, unsigned char* bw,
+	       	int width, int height);
 
 #endif
