316afc34c83144dd1dbf58c2f560875939e33e8f
Hugo C <hugo.camarasabirabent-martinez@epita.fr>
2025-10-31 03:48:15 +0100

    a bit tired

--- solver/solver.c
+++ solver/solver.c
@@ -1,73 +1,7 @@
 #include "solver.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-void solver(char* file, char* word)
-{
-	int len = 0; // len of word
-	char* w = uppercase(word, &len);
-	int* dim[2] = {0,0}; // 0 = r, 1 = c
-	*dim[0] = 0;
-	*dim[1] = 0;
-	char** grid = grid_retrieve(file, dim);
-
-	int* coords[4] = {0};
-	for(int i = 0; i < 4; i++) *coords[i] = 0;
-	// 0,1 = row,col index
-	// 2,3 = final row,col index
-	
-	// go through grid
-	short found = 0;
-	while(!found && coords[0] < dim[0] && coords[1] < dim[1])
-	{
-		if(grid[*coords[0]][*coords[1]] == w[0])
-		{
-			found = parkour(coords, dim, grid, w, len);
-		}
-		if(!found)
-		{
-			coords[1]++;
-			if(*coords[1] == *dim[1])
-			{
-				coords[1] = 0;
-				coords[0]++;
-			}
-			*coords[2] = *coords[0];
-			*coords[3] = *coords[1];
-		}
-	}
-	if(!found) printf("Not found\n");
-	else printf("(%i,%i)(%i,%i)\n", *coords[0], *coords[1], *coords[2], *coords[3]);
-}
-/*
-char* uppercase(char* word, int* len)
-{
-	char* res = word;
-	for(int i = 0; res[i] != 0; i++)
-	{
-		*len = *len + 1;
-		if(res[i] >= 'a' && res[i] <= 'z')
-			res[i] = (char)(res[i] - 32);
-	}
-	return res;
-}
-*/
-
-char* uppercase(char* w, int* len)
-{
-	for(int i = 0; w[i] != 0; i++)
-	{
-		*len = *len + 1;
-	}
-	char res[*len];
-	for(int i = 0; i < *len; i++)
-	{
-		if(w[i] >= 'a' && w[i] <= 'z')
-			res[i] = (char)(w[i] - 32);
-	}
-
-	return res;
-}
+#include <string.h>
 
 int count_lines(FILE* file)
 {
@@ -75,65 +9,133 @@ int count_lines(FILE* file)
 	int counter = 0;
 	for(;;)
 	{
-		size_t res = fread(buf, 1, 65536, file);
-		if (ferror(file))
-			return -1;
+		size_t res = fread(buf,1,65536,file);
+		if(ferror(file)) return -1;
 		size_t i;
 		for(i = 0; i < res; i++)
-			if (buf[i] == '\n')
+			if(buf[i] == '\n')
 				counter++;
-		if (feof(file))
+		if(feof(file))
 			break;
 	}
 	return counter;
 }
 
-char** grid_retrieve(char* file, int** dim)
+
+void solver(char* file, char* word)
 {
-	FILE* f = fopen(file, "r");
+	int len = 0; // len of word
+	
+	// uppercase
+	for(int i = 0; word[i] == 0; i++)
+	{
+		len++;
+		if(word[i] >= 'a' && word[i] <= 'z')
+			word[i] = (char)(word[i] - 32);
+	}
 
-	if (f == NULL)
-		errx(EXIT_FAILURE, "file open error");
 
-	*dim[0] = count_lines(f);
-	if(*dim[0] == -1)
-		errx(EXIT_FAILURE, "count_lines error");
+	int dim[2] = {0,0}; // 0 = r, 1 = c
+	dim[0] = 0;
+	dim[1] = 0;
 
-	if(fseek(f, 0, SEEK_SET) < 0)
-		errx(EXIT_FAILURE, "fseek error");
 
-	char** line[*dim[0]];
+	// grid retrieve
+	
+	FILE* fp = fopen(file, "r");
+	if(fp == NULL) printf("fopen error");
+	
+	dim[0] = count_lines(fp);
+	if(dim[0] == -1) printf("count_lines error");
+
+	if(fseek(fp, 0, SEEK_SET) < 0) printf("fseek error");
+
+	char** grid = malloc(sizeof(char)*sizeof(char)*dim[0]);
 	size_t* n = malloc(sizeof(size_t));
 	*n = 120;
-	*dim[1] = getline(line[0], n, f);
-	for(int i = 1; i < *dim[0]; i++)
+	char* line[dim[0]];
+	dim[1] = getline(line, n , fp);
+	grid[0] = malloc(sizeof(char)*dim[1]);
+	grid[0] = strcpy(grid[0], *line);
+	//getline(line, n, fp);
+	//grid[1] = strcpy(grid[1], *line);
+	for(int i = 1; i < dim[0]; i++)
 	{
-		getline(line[i], n, f);
+		getline(line, n, fp);
+		grid[i] = malloc(sizeof(char)*dim[1]);
+		grid[i] = strcpy(grid[i], *line);
 	}
-	free(n);
-	fclose(f);
-	return *line;
+	//free(n);
+	fclose(fp);	
+	
+	int coords[4] = {0};
+	for(int i = 0; i < 4; i++) coords[i] = 0;
+	// 0,1 = row,col index
+	// 2,3 = final row,col index
+	
+	// go through grid
+	short b = 0;
+	while(!b && coords[0] < dim[0] && coords[1] < dim[1])
+	{
+		if(grid[coords[0]][coords[1]] == word[0])
+		{
+			// [U,D,L,R]
+			short dir[4] = {0,0,0,0};
+			if(coords[0]<len) dir[0] = 1;
+			if(dim[0]<len+coords[0]) dir[1] = 1;
+			if(coords[1]<len) dir[2] = 1;
+			if(dim[1]<len+coords[1]) dir[3] = 1;
+			if(dir[0]) // up
+			{
+				if(dir[2])
+					b=search(4,coords,dim,grid,word,len);
+				if(!b && dir[3])
+					b=search(5,coords,dim,grid,word,len);
+				if(!b)
+					b=search(0,coords,dim,grid,word,len);
+			}
+			if(!b && dir[1]) // down
+			{
+				if(dir[2])
+					b=search(6,coords,dim,grid,word,len);
+				if(!b && dir[3])
+					b=search(7,coords,dim,grid,word,len);
+				if(!b)
+					b=search(1,coords,dim,grid,word,len);
+			}
+			if(!b && dir[2]) // left
+				b=search(2,coords,dim,grid,word,len);
+			if(!b && dir[3]) // right
+				b=search(3,coords,dim,grid,word,len);
+			
+			// SIGNATURE SEARCH
+		}
+		if(!b)
+		{
+			coords[1]++;
+			if(coords[1] == dim[1])
+			{
+				coords[1] = 0;
+				coords[0]++;
+			}
+			coords[2] = coords[0];
+			coords[3] = coords[1];
+		}
+	}
+	if(!b) printf("Not found\n");
+	else printf("(%i,%i)(%i,%i)\n",
+			coords[0], coords[1], coords[2], coords[3]);
 }
 
-short* direction(int r, int c, int** dim, int len)
-{
-	// [Up, Down, Left, Right]
-	short* res[4] = {0};
-	if(r < len) *res[0] = 1;
-	if(*dim[0] < len + r) *res[1] = 1;
-	if(c < len) *res[2] = 1;
-	if(*dim[1] < len+c) *res[3] = 1;
-	return *res;
-}
 
-short search(int dir, int** coords, int** dim, char** grid, char* word, int len)
+short search(int dir, int* coords, int* dim, char** grid, char* word, int len)
 {
 	// dir => {0:up, down, left, right, uL, uR, dL, 7:dR}
 	
 	short valid = 1;
 	
-	int r = *coords[0];
-	int c = *coords[1];
+	int r = coords[0];
+	int c = coords[1];
 	int i = 0;
 
 	switch(dir)
@@ -148,7 +150,7 @@ short search(int dir, int** coords, int** dim, char** grid, char* word, int len)
 			}
 			break;
 		case 1:
-			while(valid && r < *dim[0] && i < len)
+			while(valid && r < dim[0] && i < len)
 			{
 				valid = valid && grid[r][c] == word[i];
 				i++;
@@ -166,7 +168,7 @@ short search(int dir, int** coords, int** dim, char** grid, char* word, int len)
 			}
 			break;
 		case 3:
-			while(valid && i < len && c < *dim[1])
+			while(valid && i < len && c < dim[1])
 			{
 				valid = valid && grid[r][c] == word[i];
 				i++;
@@ -187,7 +189,7 @@ short search(int dir, int** coords, int** dim, char** grid, char* word, int len)
 			}
 			break;
 		case 5:
-			while(valid && i < len && r > 0 && c < *dim[1])
+			while(valid && i < len && r > 0 && c < dim[1])
 			{
 				valid = valid && grid[r][c] == word[i];
 				i++;
@@ -199,7 +201,7 @@ short search(int dir, int** coords, int** dim, char** grid, char* word, int len)
 			}
 			break;
 		case 6:
-			while(valid && i < len && r < *dim[0] && c > 0)
+			while(valid && i < len && r < dim[0] && c > 0)
 			{
 				valid = valid && grid[r][c] == word[i];
 				i++;
@@ -211,7 +213,7 @@ short search(int dir, int** coords, int** dim, char** grid, char* word, int len)
 			}
 			break;
 		case 7:
-			while(valid && i < len && r < *dim[0] && c < *dim[1])
+			while(valid && i < len && r < dim[0] && c < dim[1])
 			{
 				valid = valid && grid[r][c] == word[i];
 				i++;
@@ -226,48 +228,13 @@ short search(int dir, int** coords, int** dim, char** grid, char* word, int len)
 
 	if(valid)
 	{
-		*coords[2] = r;
-		*coords[3] = c;
+		coords[2] = r;
+		coords[3] = c;
 	}
 
 	return valid;
 }
 
-short parkour(int** coords, int** dim, char** grid, char* word, int len)
-// return 1 if found(with coords changed), 0 otherwise
-{
-	short found = 0;
-	short* dir = direction(*coords[0], *coords[1], dim, len);
-
-	if(dir[0]) // up
-	{
-		if(dir[2])
-			found = search(4, coords, dim, grid, word, len);
-		if(!found && dir[3])
-			found = search(5, coords, dim, grid, word, len);
-		if(!found)
-			found = search(0, coords, dim, grid, word, len);
-	}
-	if(!found && dir[1]) // down
-	{
-		if(dir[2])
-			found = search(6, coords, dim, grid, word, len);
-		if(!found && dir[3])
-			found = search(5, coords, dim, grid, word, len);
-		if(!found)
-			found = search(1, coords, dim, grid, word, len);
-	}
-	if(!found && dir[2]) // left
-	{
-		found = search(2, coords, dim, grid, word, len);
-	}
-	if(!found && dir[3]) // right
-	{
-		found = search(3, coords, dim, grid, word, len);
-	}
-
-	return found;
-}
 
 int main(int argc, char* argv[])
 {
--- solver/solver.h
+++ solver/solver.h
@@ -4,14 +4,6 @@
 
 void solver(char* file, char* word);
 
-char* uppercase(char* word, int* len);
-
-char** grid_retrieve(char* file, int** dim);
-
-short* direction(int r, int c, int** dim, int len);
-
-short search(int dir, int** coords, int** dim, char** grid, char* word, int len);
-
-short parkour(int** coords, int** dim, char** grid, char* word, int len);
+short search(int dir, int* coords, int* dim, char** grid, char* word, int len);
 
 #endif
