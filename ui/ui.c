e44035a545e8aa9dcf3d046c7552f084dfe53896
Hugo C <hugo.camarasabirabent-martinez@epita.fr>
2025-12-16 18:42:02 +0100

    fix

--- ui/ui.c
+++ ui/ui.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 
 
-/* Paths to images */
-#define INPUT_IMAGE   "data/images/input.png"
-#define ROTATED_IMAGE "data/images/02_corrected_final.png"
-#define SOLVED_IMAGE  "../ouput/output.png"
+// paths to images
+#define INPUT_IMAGE   "data/input/level_2_image_2.png"
+#define ROTATED_IMAGE "data/images/treated_image.png"
+#define SOLVED_IMAGE  "output.png"
 
-/* Global image widget */
+
 GtkWidget *image_widget;
 
-/* ---------- Button callbacks ---------- */
 
 void on_rotate_clicked(GtkButton *button, gpointer user_data)
 {
@@ -21,7 +20,6 @@ void on_rotate_clicked(GtkButton *button, gpointer user_data)
     int e = system("./pretreatment level_2_image_2.png");
     if(e != 0){ printf("rotate syst error"); return;}
 
-    /* Update displayed image */
     gtk_image_set_from_file(GTK_IMAGE(image_widget), ROTATED_IMAGE);
 }
 
@@ -34,11 +32,9 @@ void on_solve_clicked(GtkButton *button, gpointer user_data)
     int e = system("./wordsearch_solver data/images/treated_image.png");
     if(e != 0){ printf("solver syst error"); return;}
 
-    /* Display solved grid */
     gtk_image_set_from_file(GTK_IMAGE(image_widget), SOLVED_IMAGE);
 }
 
-/* ---------- Main ---------- */
 
 int main(int argc, char *argv[])
 {
@@ -51,31 +47,27 @@ int main(int argc, char *argv[])
 
     gtk_init(&argc, &argv);
 
-    /* Window */
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Word Search Solver");
     gtk_window_set_default_size(GTK_WINDOW(window), 600, 600);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    /* Vertical layout */
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    /* Image display */
     image_widget = gtk_image_new_from_file(INPUT_IMAGE);
     gtk_box_pack_start(GTK_BOX(vbox), image_widget, TRUE, TRUE, 0);
 
-    /* Horizontal box for buttons */
     hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
     gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
 
-    /* Rotate button */
+    // rotate button
     rotate_button = gtk_button_new_with_label("Rotate Image");
     g_signal_connect(rotate_button, "clicked",
                      G_CALLBACK(on_rotate_clicked), NULL);
     gtk_box_pack_start(GTK_BOX(hbox), rotate_button, TRUE, TRUE, 0);
 
-    /* Solve button */
+    // solver button
     solve_button = gtk_button_new_with_label("Solve");
     g_signal_connect(solve_button, "clicked",
                      G_CALLBACK(on_solve_clicked), NULL);
